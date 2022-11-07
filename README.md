4d0d732dab9796377464f6d8967d53370018f568
Andy Aschwanden <andy.aschwanden@gmail.com>
2022-11-07 08:32:34 -0900

    Started Dummy Project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pypac
 Python-based PISM analysis campaigns.
+
+[![License: GPL-3.0](https://img.shields.io:/github/license/pism/pypac)](https://opensource.org/licenses/GPL-3.0)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![flake8](https://img.shields.io/badge/flake8-enabled-green)](https://github.com/PyCQA/flake8)
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pylint: skip-file
+
+from pathlib import Path
+from setuptools import setup
+
+with open(str(Path(".", "VERSION").absolute())) as version_file:
+    version = version_file.read().strip()
+
+packages = ["pypac"]
+
+# Dependencies of pism-emulator
+requirements = ["configobj", "jinja2"]
+
+setup(
+    name="pypac",
+    version=version,
+    author="Andy Aschwanden",
+    author_email="andy.aschwanden@gmail.com",
+    description=("Postprocessing for PISM"),
+    license="GPL 3.0",
+    keywords="PISM",
+    url="https://github.com/pism/pypac",
+    project_urls={
+        "Bug Tracker": "https://github.com/pism/pypac/issues",
+        "Documentation": "https://github.com/pism/pypac",
+        "Source Code": "https://github.com/pism/pypac",
+    },
+    packages=packages,
+    install_requires=requirements,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Postprocessing",
+    ],
+)
