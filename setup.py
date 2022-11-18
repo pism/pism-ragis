934c97e5d0decafa03ad2893e00987f02e96cce7
Andy Aschwanden <andy.aschwanden@gmail.com>
2022-11-18 12:29:40 -0900

    added extract_profiles script
    renamed package

--- setup.py
+++ setup.py
@@ -10,27 +10,41 @@ from setuptools import setup
 with open(str(Path(".", "VERSION").absolute())) as version_file:
     version = version_file.read().strip()
 
-packages = ["pypac"]
+PKG_NAME = "pismanalysis"
 
-# Dependencies of pism-emulator
-requirements = ["configobj", "jinja2", "pre-commit"]
+packages = [PKG_NAME]
+
+# Dependencies of pism-analysis
+requirements = ["pyproj", "configobj", "jinja2"]
 
 setup(
-    name="pypac",
+    name="pismanalysis",
     version=version,
     author="Andy Aschwanden",
     author_email="andy.aschwanden@gmail.com",
     description=("Postprocessing for PISM"),
     license="GPL 3.0",
     keywords="PISM",
-    url="https://github.com/pism/pypac",
+    url="https://github.com/pism/pism-analysis",
     project_urls={
-        "Bug Tracker": "https://github.com/pism/pypac/issues",
-        "Documentation": "https://github.com/pism/pypac",
-        "Source Code": "https://github.com/pism/pypac",
+        "Bug Tracker": "https://github.com/pism/pism-analysis/issues",
+        "Documentation": "https://github.com/pism/pism-analysis",
+        "Source Code": "https://github.com/pism/pism-analysis",
     },
-    packages=packages,
+    packages=[PKG_NAME],
+    package_dir={PKG_NAME: PKG_NAME},
     install_requires=requirements,
+    extras_require={
+        "develop": [
+            "flake8",
+            "pytest",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            f"extract_profiles = {PKG_NAME}.extract_profiles:main",
+        ]
+    },
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
