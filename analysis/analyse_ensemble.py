Fixing some issues with calving rate scaling
Also we need to aggregate geometries for the retreat masks.

analysis/analyse_ensemble.py
@@ -281,12 +281,14 @@ if __name__ == "__main__":
 
     result_dir = Path(options.result_dir)
 
-    reference_year = 1992
+    reference_year = 1980
 
     ds = load_ensemble(result_dir)
     ds = standarize_variable_names(ds, ragis_config["PISM"])
     ds = normalize_cumulative_variables(
-        ds, ragis_config["Cumulative Variables"]["mass_cumulative"], reference_year=1992
+        ds,
+        ragis_config["Cumulative Variables"]["mass_cumulative"],
+        reference_year=reference_year,
     )
     config = ds.sel(basin="GIS").sel(config_axis=params).config
     uq_df = config.to_dataframe()
@@ -306,17 +308,38 @@ if __name__ == "__main__":
     imbie_2021 = load_imbie_2021()
 
     mou = load_mouginot(url=Path(options.mouginot_url), norm_year=reference_year)
-    mou_gis = mou.sel(basin="GIS")
-
-    comp = {"zlib": True, "complevel": 2}
 
     flux_vars = ragis_config["Flux Variables"]
     flux_uncertainty_vars = {
         k + "_uncertainty": v + "_uncertainty" for k, v in flux_vars.items()
     }
+    cumulative_vars = ragis_config["Cumulative Variables"]
+    cumulative_uncertainty_vars = {
+        k + "_uncertainty": v + "_uncertainty" for k, v in cumulative_vars.items()
+    }
+    imbie_mean = imbie.sel(time=slice("1992-1-1", "2012-1-1"))[
+        list(cumulative_uncertainty_vars.values())
+        + list(flux_uncertainty_vars.values())
+    ].mean()
+    mou_mean = mou.sel(time=slice("1992-1-1", "2012-1-1"))[
+        list(cumulative_uncertainty_vars.values())
+        + list(flux_uncertainty_vars.values())
+    ].mean()
+    u_ratio = imbie_mean / mou_mean / 2
+    sigma_adjusted = np.maximum(u_ratio, 1)
 
-    observed = imbie
-    simulated = ds.sel(basin="GIS", ensemble_id="RAGIS")
+    mou_adjusted = mou
+    mou_adjusted[
+        list(cumulative_uncertainty_vars.values())
+        + list(flux_uncertainty_vars.values())
+    ] *= sigma_adjusted[
+        list(cumulative_uncertainty_vars.values())
+        + list(flux_uncertainty_vars.values())
+    ]
+    observed = mou_adjusted.sel(basin="GIS")
+    mou_gis = mou_adjusted.sel(basin="GIS")
+
+    simulated = ds.sel(basin="GIS", ensemble_id="RAGIS").rolling(time=13).mean()
     simulated["ensemble_id"] = "Prior"
 
     # Apply the conversion function to each column
@@ -339,8 +362,8 @@ if __name__ == "__main__":
         resampled_ids = resample_ensemble_by_data(
             observed,
             simulated,
-            start_date="1992-01-01",
-            end_date="2012-01-01",
+            start_date="1985-01-01",
+            end_date="2015-01-01",
             fudge_factor=3,
             n_samples=len(simulated.exp_id),
             obs_mean_var=obs_mean_var,
@@ -429,18 +452,7 @@ if __name__ == "__main__":
             color=obs_cmap[0],
             alpha=0.5,
             lw=0,
-            label="Mouginot et al (2019)",
-        )
-        imbie_ci = axs[0].fill_between(
-            imbie_2021["time"],
-            imbie_2021[mass_cumulative_varname]
-            - imbie_2021[mass_cumulative_uncertainty_varname],
-            imbie_2021[mass_cumulative_varname]
-            + imbie_2021[mass_cumulative_uncertainty_varname],
-            color=obs_cmap[2],
-            alpha=0.5,
-            lw=0,
-            label="IMBIE 2021",
+            label="Mouginot adj/IMBIE",
         )
 
         axs[1].fill_between(
@@ -453,27 +465,11 @@ if __name__ == "__main__":
             alpha=0.5,
             lw=0,
         )
-        axs[1].fill_between(
-            imbie["time"],
-            imbie[discharge_flux_varname] - imbie[discharge_flux_uncertainty_varname],
-            imbie[discharge_flux_varname] + imbie[discharge_flux_uncertainty_varname],
-            color=obs_cmap[2],
-            alpha=0.5,
-            lw=0,
-        )
         axs[2].fill_between(
             mou_gis["time"],
             mou_gis[smb_flux_varname] - mou_gis[smb_flux_uncertainty_varname],
             mou_gis[smb_flux_varname] + mou_gis[smb_flux_uncertainty_varname],
             color=obs_cmap[0],
-            alpha=0.5,
-            lw=0,
-        )
-        axs[2].fill_between(
-            imbie["time"],
-            imbie[smb_flux_varname] - imbie[smb_flux_uncertainty_varname],
-            imbie[smb_flux_varname] + imbie[smb_flux_uncertainty_varname],
-            color=obs_cmap[2],
             alpha=0.5,
             lw=0,
         )
@@ -504,6 +500,7 @@ if __name__ == "__main__":
             )
             if k == 0:
                 sim_cis.append(sim_ci)
+
         quantiles = {}
         for q in [0.16, 0.5, 0.84]:
             quantiles[q] = (
@@ -519,7 +516,7 @@ if __name__ == "__main__":
                 quantiles[0.5].time,
                 quantiles[0.16][m_var],
                 quantiles[0.84][m_var],
-                alpha=0.2,
+                alpha=0.4,
                 color=sim_cmap[3],
                 label=simulated_resampled["ensemble_id"].values,
                 lw=0,
@@ -529,9 +526,7 @@ if __name__ == "__main__":
             axs[k].plot(
                 quantiles[0.5].time, quantiles[0.5][m_var], lw=1, color=sim_cmap[3]
             )
-        legend_obs = axs[0].legend(
-            handles=[mou_ci, imbie_ci], loc="lower left", title="Observed"
-        )
+        legend_obs = axs[0].legend(handles=[mou_ci], loc="lower left", title="Observed")
         legend_obs.get_frame().set_linewidth(0.0)
         legend_obs.get_frame().set_alpha(0.0)
 
@@ -546,7 +541,7 @@ if __name__ == "__main__":
         axs[0].add_artist(legend_obs)
         axs[0].add_artist(legend_sim)
 
-        axs[0].set_ylim(-6000, 3000)
+        axs[0].set_ylim(-10_000, 500)
         axs[1].set_ylim(-750, 0)
         axs[2].set_ylim(0, 750)
         axs[0].xaxis.set_tick_params(labelbottom=False)
@@ -562,6 +557,44 @@ if __name__ == "__main__":
         fig.tight_layout()
         fig.savefig(f"GIS_mass_accounting_filtered_by_{resampling_var}.pdf")
 
+    fig, axs = plt.subplots(
+        4,
+        2,
+        figsize=[6.2, 7.2],
+    )
+    fig.subplots_adjust(hspace=0.75, wspace=0.25)
+
+    for k, basin in enumerate(mou_adjusted.basin):
+        obs = mou_adjusted.sel(basin=basin)
+        sim = ds.sel(ensemble_id="RAGIS", basin=basin)
+        ax = axs.ravel()[k]
+        quantiles = {}
+        for q in [0.16, 0.5, 0.84]:
+            quantiles[q] = (
+                sim.load().drop_vars("config").quantile(q, dim="exp_id", skipna=True)
+            )
+        m_var = "cumulative_mass_balance"
+        sim_ci = ax.fill_between(
+            quantiles[0.5].time,
+            quantiles[0.16][m_var],
+            quantiles[0.84][m_var],
+            alpha=0.2,
+            color=sim_cmap[1],
+            label=simulated["ensemble_id"].values,
+            lw=0,
+        )
+        mou_ci = ax.fill_between(
+            obs["time"],
+            obs[mass_cumulative_varname] - obs[mass_cumulative_uncertainty_varname],
+            obs[mass_cumulative_varname] + obs[mass_cumulative_uncertainty_varname],
+            color=obs_cmap[0],
+            alpha=0.5,
+            lw=0,
+            label="Mouginot adj/IMBIE",
+        )
+        ax.set_title(basin.values)
+        ax.set_xlim(np.datetime64("1980-01-01"), np.datetime64("2021-01-01"))
+        fig.tight_layout()
     ensemble_df = ragis.drop(columns=["Ensemble", "exp_id"], errors="ignore")
     climate_dict = {
         v: k for k, v in enumerate(ensemble_df["surface.given.file"].unique())

data/02_prepare_calfin_retreat_mask.py
@@ -15,24 +15,177 @@
 # You should have received a copy of the GNU General Public License
 # along with PISM; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 """
 Prepare CALFIN front retreat.
 """
+# pylint: disable=unused-import
 
+import time
+import warnings
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from pathlib import Path
-from typing import Union
+from typing import Dict, Union
 
+import cf_xarray as cfxr
+import dask
 import earthaccess
+import geopandas as gp
+import numpy as np
+import pandas as pd
+import rioxarray as rxr
+import xarray as xr
+from geocube.api.core import make_geocube
+from joblib import Parallel, delayed
+from tqdm.auto import tqdm
+
+from pism_ragis.processing import tqdm_joblib
+
+gp.options.io_engine = "pyogrio"
+xr.set_options(keep_attrs=True)
+# Suppress specific warning from loky
+warnings.filterwarnings("ignore", category=UserWarning)
+
+x_min = -653000
+x_max = 879700
+y_min = -632750
+y_max = -3384350
+bbox = [x_min, y_min, x_max, y_max]
+geom = {
+    "type": "Polygon",
+    "crs": {"properties": {"name": "EPSG:3413"}},
+    "bbox": bbox,
+    "coordinates": [
+        [
+            (x_min, y_min),
+            (x_max, y_min),
+            (x_max, y_max),
+            (x_min, y_max),
+            (x_min, y_min),  # Close the loop by repeating the first point
+        ]
+    ],
+}
+
+
+def dissolve(ds, date):
+    """
+    Dissolve geometries.
+    """
+    ds = gp.GeoDataFrame(ds, crs=crs)
+    geom_valid = ds.geometry.make_valid()
+    ds.geometry = geom_valid
+    ds = ds.dissolve()
+    ds["Date"] = date
+    ds = ds.set_index("Date")
+    return ds
+
+
+def aggregate(n, df):
+    """
+    Aggregate geometries.
+    """
+    if n == 0:
+        return df.iloc[[n]]
+    else:
+        geom = df.iloc[range(n)].unary_union
+        merged_df = df.iloc[[n]]
+        merged_df.iloc[0].geometry = geom
+        return merged_df
+
+
+def create_ds(
+    date: pd.Timestamp,
+    ds1: gp.GeoDataFrame,
+    ds2: gp.GeoDataFrame,
+    geom: Dict,
+    resolution: float = 450,
+    crs: str = "EPSG:3413",
+    result_dir: Union[Path, str] = "front_retreat",
+    encoding_time: Dict = {
+        "time": {"units": "hours since 1972-01-01 00:00:00", "calendar": "standard"}
+    },
+) -> Path:
+    """
+    Create a dataset representing land ice area fraction retreat and save it to a NetCDF file.
+
+    Parameters
+    ----------
+    date : pd.Timestamp
+        The date for which the dataset is created.
+    ds1 : gp.GeoDataFrame
+        The first GeoDataFrame containing the initial geometries.
+    ds2 : gp.GeoDataFrame
+        The second GeoDataFrame containing the geometries to be compared.
+    geom : Dict
+        The geometry dictionary for the geocube.
+    resolution : float, optional
+        The resolution of the geocube, by default 450.
+    crs : str, optional
+        The coordinate reference system, by default "EPSG:3413".
+    result_dir : Union[Path, str], optional
+        The directory where the result NetCDF file will be saved, by default "front_retreat".
+    encoding_time : Dict, optional
+        The encoding settings for the time variable, by default {"time": {"units": "hours since 1972-01-01 00:00:00", "calendar": "standard"}}.
+
+    Returns
+    -------
+    Path
+        The path to the saved NetCDF file.
+
+    Examples
+    --------
+    >>> import geopandas as gp
+    >>> import pandas as pd
+    >>> from pathlib import Path
+    >>> date = pd.Timestamp("2023-01-01")
+    >>> ds1 = gp.read_file("path_to_ds1.shp")
+    >>> ds2 = gp.read_file("path_to_ds2.shp")
+    >>> geom = {"type": "Polygon", "coordinates": [[[...]]]}
+    >>> result_path = create_ds(date, ds1, ds2, geom)
+    >>> print(result_path)
+    """
+    ds = gp.GeoDataFrame(ds1, crs=crs)
+    geom_valid = ds.geometry.make_valid()
+    ds.geometry = geom_valid
+    ds_dissolved = ds.dissolve()
+    diff = ds2.difference(ds_dissolved.buffer(5))
+    n = len(diff)
+    diff_df = {"land_ice_area_fraction_retreat": np.ones(n)}
+    diff_gp = gp.GeoDataFrame(data=diff_df, geometry=diff, crs=crs)
+    ds = make_geocube(
+        vector_data=diff_gp, geom=geom, resolution=(resolution, resolution)
+    )
+    ds = ds.fillna(0)
+    ds.land_ice_area_fraction_retreat["units"] = "1"
+
+    start = date.replace(day=1)
+
+    ds = ds.expand_dims(time=[start])
+    p = Path(result_dir)
+    p.mkdir(parents=True, exist_ok=True)
+
+    comp = {"zlib": True, "complevel": 2}
+    encoding = {var: comp for var in ds.data_vars}
+    encoding.update(encoding_time)
+
+    fn = p / Path(
+        f"frontretreat_g{resolution}m_{start.year}-{start.month}-{start.day}.nc"
+    )
+    ds.to_netcdf(fn, encoding=encoding)
+    return fn
 
 
 def download_calfin(
     doi: str = "10.5067/7FILV218JZA2",
     filter_str: str = "Greenland_polygons",
-    result_dir: Union[Path, str] = ".",
+    result_dir: Union[Path, str] = "calfin",
 ):
     """
     Download CALFIN.
     """
+    p = Path(result_dir)
+    p.mkdir(parents=True, exist_ok=True)
+
     earthaccess.login()
     result = earthaccess.search_data(doi=doi)
     result_filtered = [
@@ -40,8 +193,114 @@ def download_calfin(
         for granule in result
         if filter_str in granule["umm"]["DataGranule"]["Identifiers"][0]["Identifier"]
     ]
-    earthaccess.download(result_filtered, result_dir)
+    earthaccess.download(result_filtered, p)
 
 
 if __name__ == "__main__":
-    download_calfin()
+    __spec__ = None
+
+    # set up the option parser
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.description = "Prepare retreat mask from CALFIN."
+    parser.add_argument(
+        "--crs",
+        help="""Coordinate reference system. Default is EPSG:3413.""",
+        type=str,
+        default="EPSG:3413",
+    )
+    parser.add_argument(
+        "--resolution",
+        help="""Raster resolution. Default is 450.""",
+        type=int,
+        default=450,
+    )
+    parser.add_argument(
+        "--n_jobs", help="""Number of parallel jobs.""", type=int, default=4
+    )
+    options = parser.parse_args()
+
+    # download_calfin()
+
+    crs = options.crs
+    encoding = {
+        "time": {"units": "hours since 1972-01-01 00:00:00", "calendar": "standard"}
+    }
+    resolution = options.resolution
+    n_jobs = options.n_jobs
+
+    print("Preparing datasets")
+    imbie = gp.read_file("imbie/GRE_Basins_IMBIE2_v1.3_w_shelves.gpkg").to_crs(crs)
+    calfin = gp.read_file(
+        "calfin/termini_1972-2019_Greenland_polygons_v1.0.shp"
+    ).to_crs(crs)
+
+    date = pd.DatetimeIndex(calfin["Date"])
+    calfin["Date"] = date
+    calfin = calfin.set_index(date).sort_index()
+
+    geom_valid = calfin.geometry.make_valid()
+    calfin.geometry = geom_valid
+    calfin_dissolved = calfin.dissolve()
+
+    imbie_dissolved = imbie.dissolve()
+    imbie_union = imbie_dissolved.union(calfin_dissolved)
+
+    def dissolve_range(df, k):
+        """
+        Dissolve over a given range.
+        """
+        return df.reset_index().iloc[range(k)].dissolve(aggfunc="last")
+
+    n_calfin_grouped = len(
+        [date for date, df in calfin.groupby(by=pd.Grouper(freq="ME")) if len(df) > 0]
+    )
+    with tqdm_joblib(
+        tqdm(desc="Grouping geometries", total=n_calfin_grouped)
+    ) as progress_bar:
+        result = Parallel(n_jobs=n_jobs)(
+            delayed(dissolve)(ds, date)
+            for date, ds in calfin.groupby(by=pd.Grouper(freq="ME"))
+            if len(ds) > 0
+        )
+    calfin_grouped = pd.concat(result)
+
+    with tqdm_joblib(
+        tqdm(desc="Dissolving geometries", total=n_calfin_grouped)
+    ) as progress_bar:
+        result = Parallel(n_jobs=n_jobs)(
+            delayed(dissolve_range)(calfin_grouped, k)
+            for k in range(len(calfin_grouped))
+        )
+
+    calfin_aggregated = pd.concat(result[1::]).set_index("Date")
+    n_calfin_aggregated = len(calfin_aggregated)
+    with tqdm_joblib(
+        tqdm(desc="Aggregating geometries", total=n_calfin_aggregated)
+    ) as progress_bar:
+        result = Parallel(n_jobs=n_jobs)(
+            delayed(create_ds)(date, ds, imbie_union, geom=geom, resolution=resolution)
+            for date, ds in calfin_aggregated.groupby(by=pd.Grouper(freq="ME"))
+            if len(ds) > 0
+        )
+
+    p = Path("front_retreat")
+    fn = Path(f"pism_g{resolution}m_frontretreat_calfin_1972_2019.nc")
+    p_fn = p / fn
+
+    print(f"Merging datasets and saving as {p_fn.name}")
+
+    result_filtered = [element for element in result if element is not None]
+
+    start = time.time()
+    ds = xr.open_mfdataset(result_filtered).load()
+    ds = ds.cf.add_bounds("time")
+
+    comp = {"zlib": True, "complevel": 2}
+    encoding_compression = {var: comp for var in ds.data_vars}
+    encoding.update(encoding_compression)
+
+    ds.to_netcdf(p_fn, encoding=encoding)
+
+    end = time.time()
+    time_elapsed = end - start
+    print(f"Time elapsed {time_elapsed:.0f}s")

uq/draw_ragis_samples.py
@@ -58,9 +58,9 @@ dists: Dict[str, Any] = {
     "ragis": {
         "uq": {
             "calving.vonmises_calving.sigma_max": uniform(loc=350_000, scale=300_000),
+            "calving.rate_scaling.file": randint(0, 8),
             "ocean.th.gamma_T": uniform(loc=0.75e-4, scale=0.75e-4),
             "ocean_file": randint(0, len(gcms)),
-            "calving_file": randint(0, 7),
             "climate_file": randint(0, 2),
             "frontal_melt.routing.parameter_a": uniform(loc=2.4e-4, scale=1.2e-4),
             "frontal_melt.routing.parameter_b": uniform(loc=1.0, scale=0.70),
@@ -211,7 +211,7 @@ dist_sample = np.zeros_like(unif_sample, dtype="object")
 sb_dict = {0: "ssa+sia", 1: "blatter"}
 # For each variable, transform with the inverse of the CDF (inv(CDF)=ppf)
 for i, key in enumerate(keys_prior):
-    if key == "calving_rate_scaling_file":
+    if key == "calving.rate_scaling.file":
         dist_sample[:, i] = [
             f"seasonal_calving_{int(id)}_1975_2025.nc"
             for id in distributions[key].ppf(unif_sample[:, i])